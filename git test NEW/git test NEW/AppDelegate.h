2cf0749c4b5a8bc2f194a9a5f3bd8f9f40e1c97d
Bruce Yan <byan@hmc.edu>
2014-02-14 21:45:54 -0800

    added big git test in central claremont-books

---
+++ git test NEW/git test NEW/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  git test NEW
+//
+//  Created by Bruce on 2/14/14.
+//  Copyright (c) 2014 claremont-books. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
