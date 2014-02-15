2163b3095238bbac301f167eec1e7afd4d1e9d1d
christopheriksen <ceriksen@hmc.edu>
2014-02-15 15:48:14 -0800

    testing Xcode project types

---
+++ test/test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by Chris on 2/15/14.
+//  Copyright (c) 2014 cs121. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ test/test/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  test
+//
+//  Created by Chris on 2/15/14.
+//  Copyright (c) 2014 cs121. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
