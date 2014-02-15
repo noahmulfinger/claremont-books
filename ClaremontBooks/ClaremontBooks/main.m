64b15969aea3df465c7f3581f8157fb4b2744932
christopheriksen <ceriksen@hmc.edu>
2014-02-15 15:55:29 -0800

    Created master application project to hold app's code

---
+++ ClaremontBooks/ClaremontBooks/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ClaremontBooks
+//
+//  Created by Chris on 2/15/14.
+//  Copyright (c) 2014 cs121. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
