fe59bc638ab245d3d353386f265f869a47eb5d6f
Bruce Yan <byan@hmc.edu>
2014-03-03 01:40:25 -0800

    fixed modify

--- html/includes/functions.php
+++ html/includes/functions.php
@@ -67,7 +67,7 @@ class Books {
 				echo "<td>$isbn</td>";
 				echo "<td>$edition</td>";
 				echo "<td>$binding</td>";
-                echo "<td><form action=\"modify.php\" method=\"post\">
+                echo "<td><form action=\"modify.php?bookid=$bookid\">
                 <input type=\"hidden\" name=\"bookid\" value=\"$bookid\" />
                 <input type=\"submit\" name=\"submit\" value=\"Modify!\" />
                 </form>";
@@ -182,7 +182,6 @@ class Books {
                     <input type=\"hidden\" name=\"bookid\" value=\"$bookid\" />
                     <input type=\"submit\" name=\"submit\" value=\"Delete!\" />
                     </form></td>";
-    //              echo "<td><a href=\"modify.php?bookid=$bookid\">Edit</a>&nbsp<a href=\"delete.php?bookid=$bookid\">Delete</a></td>";
                     echo "</tr>";
                 }
 
--- html/modify.php
+++ html/modify.php
@@ -15,25 +15,10 @@ include 'includes/functions.php';
 $api = new Books;
 
 // Grab bookid variable from URL
-$bookid = $_POST['bookid'];
-echo "bookid is: ".$bookid."<br />";
+$bookid = $_GET['bookid'];
 
-// ensure that people got to this form from the correct place
-if (!$_POST['submit']) {
-	// Immediatebly takes user back to listing of books
-	//echo '<META HTTP-EQUIV=REFRESH CONTENT="0; '."URL=http://www.claremontbooks.com/books.php".'">';
-	echo "Where did you come from?";
-	exit;
-} else {
+$api->grabCurrentBook($bookid, $title, $author, $isbn, $edition, $binding);
 
-	$api->grabCurrentBook($bookid, $title, $author, $isbn, $edition, $binding);
-	echo "bookid is: ".$bookid."<br />";
-	echo "author is: ".$author."<br />";
-	echo "isbn is: ".$isbn."<br />";
-	echo "edition is: ".$edition."<br />";
-	echo "binding is: ".$binding."<br />";
-
-}
 ?>
 
 <h3> Modifying Book Data </h3>
@@ -50,9 +35,9 @@ if (!$_POST['submit']) {
 </form>
 
 <?php
-	if(isset($_POST['submit']) && isset($_POST['bookid'])) {
+	if(isset($_POST['submit'])) {
 		$api->modifyBook($_POST[bookid], $_POST[bookTitle], $_POST[bookAuthor], $_POST[bookISBN], $_POST[bookEdition], $_POST[bookBinding]);
-		//echo '<META HTTP-EQUIV=REFRESH CONTENT="0; '."URL=http://www.claremontbooks.com/books.php".'">';
+		echo '<META HTTP-EQUIV=REFRESH CONTENT="0; '."URL=http://www.claremontbooks.com/books.php".'">';
 		exit;
 	}
 
