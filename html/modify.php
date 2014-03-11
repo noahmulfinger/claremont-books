9dd4a08f6de91dbb6c51cad94ab98a4aa5b02d35
Noah <noah.mulfinger@gmail.com>
2014-03-11 00:25:25 -0700

    Fixed modify.php, and some Xcode files changed because I opened the project

--- html/modify.php
+++ html/modify.php
@@ -10,7 +10,6 @@
  */
 include 'includes/functions.php';
 
-if(isset($_POST['submit'])) {
 
 // This is the first thing that gets called when this page is loaded
 // Creates a new instance of the Books class
@@ -19,30 +18,37 @@ $api = new Books;
 // Grab bookid variable from URL
 $bookid = $_GET['bookid'];
 
-$api->grabCurrentBook($bookid, $title, $author, $isbn, $edition, $binding);
+//if($bookid == NULL) {
+//	header("Location: books.php");
+	//echo "goes here";
+//} else {
 
-?>
 
-<h3> Modifying Book Data </h3>
-<form action=<?php echo $_SERVER['PHP_SELF']; ?> method="post">
-	Title: <input type="text" name="bookTitle" value="<?php echo $title; ?>" /><br />
-	Author: <input type="text" name="bookAuthor" value="<?php echo $author; ?>" /><br />
-	ISBN: <input type="text" name="bookISBN" value="<?php echo $isbn; ?>" /><br />
-	Edition: <input type="text" name="bookEdition" value="<?php echo $edition; ?>" /><br />
-	Binding: <input type="text" name="bookBinding" value="<?php echo $binding; ?>" /><br />
-	<br />
+	$api->grabCurrentBook($bookid, $title, $author, $isbn, $edition, $binding);
 
-	<input type="hidden" name="bookid" value="<?php echo $bookid; ?>" />
-	<input type="submit" name="submit" value="Modify!" />
-</form>
+	?>
+
+	<h3> Modifying Book Data </h3>
+	<form action=<?php echo $_SERVER['PHP_SELF']; ?> method="post">
+		Title: <input type="text" name="bookTitle" value="<?php echo $title; ?>" /><br />
+		Author: <input type="text" name="bookAuthor" value="<?php echo $author; ?>" /><br />
+		ISBN: <input type="text" name="bookISBN" value="<?php echo $isbn; ?>" /><br />
+		Edition: <input type="text" name="bookEdition" value="<?php echo $edition; ?>" /><br />
+		Binding: <input type="text" name="bookBinding" value="<?php echo $binding; ?>" /><br />
+		<br />
+
+		<input type="hidden" name="bookid" value="<?php echo $bookid; ?>" />
+		<input type="submit" name="submit" value="Modify!" />
+	</form>
 
 <?php
-	
-	$api->modifyBook($_POST[bookid], $_POST[bookTitle], $_POST[bookAuthor], $_POST[bookISBN], $_POST[bookEdition], $_POST[bookBinding]);
-	echo '<META HTTP-EQUIV=REFRESH CONTENT="0; '."URL=http://www.claremontbooks.com/books.php".'">';
-	exit;
-} else {
-	header("Location: books.php");
-}
+
+	if(isset($_POST['submit'])) {
+		$api->modifyBook($_POST[bookid], $_POST[bookTitle], $_POST[bookAuthor], $_POST[bookISBN], $_POST[bookEdition], $_POST[bookBinding]);
+		echo '<META HTTP-EQUIV=REFRESH CONTENT="0; '."URL=http://www.claremontbooks.com/books.php".'">';
+
+		exit;
+	}
+//}
 
 ?>
