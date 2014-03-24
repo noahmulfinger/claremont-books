d5bf030a5b2596ba8a269bcecf2ae938dd6eeace
Noah <noah.mulfinger@gmail.com>
2014-03-23 17:09:10 -0700

    Minor changes to json login, changed ftp to sftp

--- html/loginjson.php
+++ html/loginjson.php
@@ -1,10 +1,16 @@
 <?php
+
+
 header('Content-type: application/json');
+
 if($_POST) {
-    if($_POST['username'] == 'dipinkrishna' && $_POST['password'] == 'password') {
-    echo '{"success":1}';
- } else {
+    
+	if($_POST['email'] == 'dipinkrishna' && $_POST['password'] == 'password') {
+    	echo '{"success":1}';
+ 	} else {
+    	echo '{"success":0,"error_message":"Username and/or password is invalid."}';
+	}
+} else {
     echo '{"success":0,"error_message":"Username and/or password is invalid."}';
 }
-}else {    echo '{"success":0,"error_message":"Username and/or password is invalid."}';}
 ?>
