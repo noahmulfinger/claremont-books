dcd3d0c95a3067e650a471b2ac7bcae7ee143705
Noah <noah.mulfinger@gmail.com>
2014-03-29 16:04:18 -0700

    finish modifying json login from tutorial to fit our project

--- html/loginjson.php
+++ html/loginjson.php
@@ -4,13 +4,28 @@
 header('Content-type: application/json');
 
 if($_POST) {
-    
-	if($_POST['email'] == 'dipinkrishna' && $_POST['password'] == 'password') {
-    	echo '{"success":1}';
- 	} else {
+	if(isset($_POST['uemail'])) {
+		if(isset($_POST['password'])) {
+
+			$uemail = $_POST['uemail'];
+    		$password = $_POST['password'];
+
+    		$user = $api->checkLoginInfo($uemail, $password);
+
+    		if($user != NULL) {
+        		$_SESSION['user'] = $user['uid'];
+
+        		echo '{"success":1}';
+			} else {
+        		echo '{"success":0,"error_message":"Username and/or password is invalid."}';
+    		}
+		} else {
+    		echo '{"success":0,"error_message":"Username and/or password is invalid."}';
+		}
+	} else {
     	echo '{"success":0,"error_message":"Username and/or password is invalid."}';
 	}
 } else {
-    echo '{"success":0,"error_message":"Username and/or password is invalid."}';
+	echo '{"success":0,"error_message":"Username and/or password is invalid."}';
 }
 ?>
